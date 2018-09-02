2c1292ca62edd0baf75748b966816195246954e1
Anthony Flora <anthonyflora@gmail.com>
2018-09-02 12:38:58 +1200

    Bridge

--- SetupGateway.sh
+++ SetupGateway.sh
@@ -12,7 +12,8 @@ sudo systemctl stop hostapd
 echo '\n*** UPDATING CONFIG dhcpcd.conf ***'
 sudo bash -c 'echo "interface wlan0" >> /etc/dhcpcd.conf'
 sudo bash -c 'echo "  static ip_address=192.168.20.1/24" >> /etc/dhcpcd.conf'
-sudo bash -c 'echo "  nohook wpa_supplicant" >> /etc/dhcpcd.conf'
+sudo bash -c 'echo "  denyinterfaces wlan0" >> /etc/dhcpcd.conf'
+sudo bash -c 'echo "  denyinterfaces wlan1" >> /etc/dhcpcd.conf'
 
 echo '\n*** STARTING SERVICES ***'
 sudo service dhcpcd restart
@@ -20,14 +21,12 @@ sudo service dhcpcd restart
 echo '\n*** UPDATING CONFIG dnsmasq.conf ***'
 sudo bash -c 'echo "" >> /etc/dnsmasq.conf'
 sudo bash -c 'echo "interface=wlan0" >> /etc/dnsmasq.conf'
-sudo bash -c 'echo "usually wlan0" >> /etc/dnsmasq.conf'
-sudo bash -c 'echo "  dhcp-range=192.168.20.2,192.168.20.100,255.255.255.0,24h" >> /etc/dnsmasq.conf'
+sudo bash -c 'echo "  dhcp-range=192.168.20.2,192.168.20.30,255.255.255.0,24h" >> /etc/dnsmasq.conf'
 
 echo '\n*** UPDATING CONFIG hostapd.conf ***'
 sudo bash -c 'echo "" >> /etc/hostapd/hostapd.conf'
 sudo bash -c 'echo "interface=wlan0" >> /etc/hostapd/hostapd.conf'
-sudo bash -c 'echo "driver=nl80211" >> /etc/hostapd/hostapd.conf'
-sudo bash -c 'echo "ssid=pi-gateway" >> /etc/hostapd/hostapd.conf'
+sudo bash -c 'echo "bridge=br0" >> /etc/hostapd/hostapd.conf'
 sudo bash -c 'echo "hw_mode=g" >> /etc/hostapd/hostapd.conf'
 sudo bash -c 'echo "channel=7" >> /etc/hostapd/hostapd.conf'
 sudo bash -c 'echo "wmm_enabled=0" >> /etc/hostapd/hostapd.conf'
@@ -35,25 +34,30 @@ sudo bash -c 'echo "macaddr_acl=0" >> /etc/hostapd/hostapd.conf'
 sudo bash -c 'echo "auth_algs=1" >> /etc/hostapd/hostapd.conf'
 sudo bash -c 'echo "ignore_broadcast_ssid=0" >> /etc/hostapd/hostapd.conf'
 sudo bash -c 'echo "wpa=2" >> /etc/hostapd/hostapd.conf'
-sudo bash -c 'echo "wpa_passphrase=computer" >> /etc/hostapd/hostapd.conf'
 sudo bash -c 'echo "wpa_key_mgmt=WPA-PSK" >> /etc/hostapd/hostapd.conf'
 sudo bash -c 'echo "wpa_pairwise=TKIP" >> /etc/hostapd/hostapd.conf'
 sudo bash -c 'echo "rsn_pairwise=CCMP" >> /etc/hostapd/hostapd.conf'
+sudo bash -c 'echo "ssid=pi-gateway" >> /etc/hostapd/hostapd.conf'
+sudo bash -c 'echo "wpa_passphrase=computer" >> /etc/hostapd/hostapd.conf'
 
 echo '\n*** UPDATING CONFIG hostapd ***'
 sudo bash -c 'echo "" >> /etc/default/hostapd'
 sudo bash -c 'echo "DAEMON_CONF=\"/etc/hostapd/hostapd.conf\"" >> /etc/default/hostapd'
 
-echo '\n*** STARTING SERVICES ***'
-sudo systemctl start hostapd
-sudo systemctl start dnsmasq
-
-echo '\n*** SETTING UP ROUTES ***'
+echo '\n*** SETTING UP FORWARDING ***'
 sudo bash -c 'echo "" >> /etc/sysctl.conf'
 sudo bash -c 'echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf'
 sudo iptables -t nat -A POSTROUTING -o wlan1 -j MASQUERADE
 sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
 sudo sed -i "\$i iptables-restore < /etc/iptables.ipv4.nat" /etc/rc.local
+
+echo '\n*** UPDATING BRIDGE ***'
+echo 'y' | sudo apt-get install bridge-utils
+sudo brctl addbr br0
+sudo brctl addif br0 wlan1
+sudo bash -c 'echo "auto br0" >> /etc/network/interfaces'
+sudo bash -c 'echo "iface br0 inet manual" >> /etc/network/interfaces'
+sudo bash -c 'echo "bridge_ports wlan1 wlan0" >> /etc/network/interfaces'
 
 echo '\n*** REBOOTING ***'
 sudo reboot
