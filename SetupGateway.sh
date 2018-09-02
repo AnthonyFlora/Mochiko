2f632623f2000e0c3ac57560f5efa86feddda3c6
Anthony Flora <anthonyflora@gmail.com>
2018-09-02 17:08:05 +1200

    Retry

--- SetupGateway.sh
+++ SetupGateway.sh
@@ -1,4 +1,10 @@
 
+# Run as: 'sudo nohup ./SetupGateway.sh &'
+# Goal: 
+#  wlan0 provides configuration AP
+#  wlan1 connects to external WIFI source
+#  eth0 provides 
+
 echo '\n*** INSTALLING PACKAGES ***'
 sudo apt-get update
 echo 'y' | sudo apt-get upgrade
@@ -12,8 +18,7 @@ sudo systemctl stop hostapd
 echo '\n*** UPDATING CONFIG dhcpcd.conf ***'
 sudo bash -c 'echo "interface wlan0" >> /etc/dhcpcd.conf'
 sudo bash -c 'echo "  static ip_address=192.168.20.1/24" >> /etc/dhcpcd.conf'
-sudo bash -c 'echo "  denyinterfaces wlan0" >> /etc/dhcpcd.conf'
-sudo bash -c 'echo "  denyinterfaces wlan1" >> /etc/dhcpcd.conf'
+sudo bash -c 'echo "  static routers=192.168.20.0" >> /etc/dhcpcd.conf'
 
 echo '\n*** STARTING SERVICES ***'
 sudo service dhcpcd restart
@@ -21,12 +26,16 @@ sudo service dhcpcd restart
 echo '\n*** UPDATING CONFIG dnsmasq.conf ***'
 sudo bash -c 'echo "" >> /etc/dnsmasq.conf'
 sudo bash -c 'echo "interface=wlan0" >> /etc/dnsmasq.conf'
+sudo bash -c 'echo "  listen-address=192.168.20.1" >> /etc/dnsmasq.conf'
+sudo bash -c 'echo "  bind-interfaces" >> /etc/dnsmasq.conf'
+sudo bash -c 'echo "  server=8.8.8.8" >> /etc/dnsmasq.conf'
+sudo bash -c 'echo "  domain-needed" >> /etc/dnsmasq.conf'
+sudo bash -c 'echo "  bogus-priv" >> /etc/dnsmasq.conf'
 sudo bash -c 'echo "  dhcp-range=192.168.20.2,192.168.20.30,255.255.255.0,24h" >> /etc/dnsmasq.conf'
 
 echo '\n*** UPDATING CONFIG hostapd.conf ***'
 sudo bash -c 'echo "" >> /etc/hostapd/hostapd.conf'
 sudo bash -c 'echo "interface=wlan0" >> /etc/hostapd/hostapd.conf'
-sudo bash -c 'echo "bridge=br0" >> /etc/hostapd/hostapd.conf'
 sudo bash -c 'echo "hw_mode=g" >> /etc/hostapd/hostapd.conf'
 sudo bash -c 'echo "channel=7" >> /etc/hostapd/hostapd.conf'
 sudo bash -c 'echo "wmm_enabled=0" >> /etc/hostapd/hostapd.conf'
@@ -48,16 +57,11 @@ echo '\n*** SETTING UP FORWARDING ***'
 sudo bash -c 'echo "" >> /etc/sysctl.conf'
 sudo bash -c 'echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf'
 sudo iptables -t nat -A POSTROUTING -o wlan1 -j MASQUERADE
+sudo iptables -A FORWARD -i wlan1 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i wlan0 -o wlan1 -j ACCEPT
 sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
 sudo sed -i "\$i iptables-restore < /etc/iptables.ipv4.nat" /etc/rc.local
 
-echo '\n*** UPDATING BRIDGE ***'
-echo 'y' | sudo apt-get install bridge-utils
-sudo brctl addbr br0
-sudo brctl addif br0 wlan1
-sudo bash -c 'echo "auto br0" >> /etc/network/interfaces'
-sudo bash -c 'echo "iface br0 inet manual" >> /etc/network/interfaces'
-sudo bash -c 'echo "bridge_ports wlan1 wlan0" >> /etc/network/interfaces'
 
 echo '\n*** REBOOTING ***'
 sudo reboot
