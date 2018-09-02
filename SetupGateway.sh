6c47d6fda7f91e984d60d20fda978d46ea93979d
Anthony Flora <anthonyflora@gmail.com>
2018-09-02 19:54:54 +1200

    Working SetupGateway

--- SetupGateway.sh
+++ SetupGateway.sh
@@ -18,24 +18,22 @@ sudo systemctl stop hostapd
 echo '\n*** UPDATING CONFIG dhcpcd.conf ***'
 sudo bash -c 'echo "interface wlan0" >> /etc/dhcpcd.conf'
 sudo bash -c 'echo "  static ip_address=192.168.20.1/24" >> /etc/dhcpcd.conf'
-sudo bash -c 'echo "  static routers=192.168.20.0" >> /etc/dhcpcd.conf'
+sudo bash -c 'echo "  nohook wpa_supplicant" >> /etc/dhcpcd.conf'
 
 echo '\n*** STARTING SERVICES ***'
 sudo service dhcpcd restart
 
 echo '\n*** UPDATING CONFIG dnsmasq.conf ***'
-sudo bash -c 'echo "" >> /etc/dnsmasq.conf'
+sudo bash -c 'echo "" > /etc/dnsmasq.conf'
+sudo bash -c 'echo "expand-hosts" >> /etc/dnsmasq.conf'
 sudo bash -c 'echo "interface=wlan0" >> /etc/dnsmasq.conf'
-sudo bash -c 'echo "  listen-address=192.168.20.1" >> /etc/dnsmasq.conf'
-sudo bash -c 'echo "  bind-interfaces" >> /etc/dnsmasq.conf'
-sudo bash -c 'echo "  server=8.8.8.8" >> /etc/dnsmasq.conf'
-sudo bash -c 'echo "  domain-needed" >> /etc/dnsmasq.conf'
-sudo bash -c 'echo "  bogus-priv" >> /etc/dnsmasq.conf'
-sudo bash -c 'echo "  dhcp-range=192.168.20.2,192.168.20.30,255.255.255.0,24h" >> /etc/dnsmasq.conf'
+sudo bash -c 'echo "authoritative" >> /etc/dnsmasq.conf'
+sudo bash -c 'echo "dhcp-range=192.168.20.2,192.168.20.30,255.255.255.0,24h" >> /etc/dnsmasq.conf'
 
 echo '\n*** UPDATING CONFIG hostapd.conf ***'
 sudo bash -c 'echo "" >> /etc/hostapd/hostapd.conf'
 sudo bash -c 'echo "interface=wlan0" >> /etc/hostapd/hostapd.conf'
+sudo bash -c 'echo "driver=nl80211" >> /etc/hostapd/hostapd.conf'
 sudo bash -c 'echo "hw_mode=g" >> /etc/hostapd/hostapd.conf'
 sudo bash -c 'echo "channel=7" >> /etc/hostapd/hostapd.conf'
 sudo bash -c 'echo "wmm_enabled=0" >> /etc/hostapd/hostapd.conf'
@@ -53,15 +51,18 @@ echo '\n*** UPDATING CONFIG hostapd ***'
 sudo bash -c 'echo "" >> /etc/default/hostapd'
 sudo bash -c 'echo "DAEMON_CONF=\"/etc/hostapd/hostapd.conf\"" >> /etc/default/hostapd'
 
+echo '\n*** STARTING SERVICES hostapd dnsmasq ***'
+sudo systemctl start hostapd
+sudo systemctl start dnsmasq
+
 echo '\n*** SETTING UP FORWARDING ***'
 sudo bash -c 'echo "" >> /etc/sysctl.conf'
 sudo bash -c 'echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf'
-sudo iptables -t nat -A POSTROUTING -o wlan1 -j MASQUERADE
-sudo iptables -A FORWARD -i wlan1 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-sudo iptables -A FORWARD -i wlan0 -o wlan1 -j ACCEPT
+sudo iptables -I INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -I FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -t nat -I POSTROUTING -o wlan1 -j MASQUERADE
 sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
 sudo sed -i "\$i iptables-restore < /etc/iptables.ipv4.nat" /etc/rc.local
-
 
 echo '\n*** REBOOTING ***'
 sudo reboot
