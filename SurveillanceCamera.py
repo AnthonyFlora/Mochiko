60703658005c62c59a27b176b37cb4c502678dc4
Anthony Flora <anthonyflora@gmail.com>
2021-07-04 20:41:00 -0400

    Fix

--- SurveillanceCamera.py
+++ SurveillanceCamera.py
@@ -45,7 +45,7 @@ class SurveillanceCamera(Service.Service):
 
     def write(self, buf):
         if buf.startswith(b'\xff\xd8'):
-            self.log('writing')
+            self.log('frame received next=%0.6f curr=%0.6f' % (self.time_next_snapshot, time.time()))
             if not self.time_next_snapshot:
                 self.time_next_snapshot = time.time()
             if self.time_next_snapshot <= time.time():
