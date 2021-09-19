c37dbc6177fdcad40d9b3ac8c7235c9eca033b90
Anthony Flora <anthonyflora@gmail.com>
2021-09-19 19:31:55 -0400

    stream

--- surveillance/SurveillanceService.py
+++ surveillance/SurveillanceService.py
@@ -19,7 +19,7 @@ class SurveillanceService(Service.Service):
         self.topic_sensor_status = 'surveillance/%s/status' % self.hostname
         self.topic_sensor_stream = 'surveillance/%s/stream' % self.hostname
         self.config = defaultdict(lambda: None)
-        self.config['fps'] = 12
+        self.config['fps'] = .1
         self.config['res_x'] = 320
         self.config['res_y'] = 240
         self.is_reconfig_needed = False
