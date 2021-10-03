782899a950223f7f62b19083b83daf5c6a05c458
Anthony Flora <anthonyflora@gmail.com>
2021-10-02 22:51:30 -0400

    Added GTK Display

--- python/DisplayGtk.py
+++ python/DisplayGtk.py
@@ -1,134 +1,79 @@
 #!/usr/bin/env python
 
-import cairo
+import Config
+import paho.mqtt.client as mqtt
 import gi
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk, Gdk, GObject, GLib
+from gi.repository import Gtk, GdkPixbuf
+from threading import Thread
+
+# -----------------------------------------------------------------------------
 
 
-class Screen(Gtk.DrawingArea):
-    """ This class is a Drawing Area"""
+class Display(Gtk.Window):
+
     def __init__(self):
-        super(Screen,self).__init__()
-        ## Connect to the "draw" signal
-        self.connect("draw", self.on_draw)
-        ## This is what gives the animation life!
-        GLib.timeout_add(50, self.tick) # Go call tick every 50 whatsits.
+        super(Display, self).__init__()
+        self.set_title('Display')
+        self.set_size_request(320, 240)
+        self.image = Gtk.Image()
+        self.add(self.image)
+        self.connect('destroy', Gtk.main_quit)
+        self.show_all()
 
-    def tick(self):
-        ## This invalidates the screen, causing the "draw" event to fire.
-        rect = self.get_allocation()
-        self.get_window().invalidate_rect(rect, True)
-        return True # Causes timeout to tick again.
+    def set_image(self, pixbuf):
+        self.image.set_from_pixbuf(pixbuf)
 
-    ## When the "draw" event fires, this is run
-    def on_draw(self, widget, event):
-        self.cr = self.get_window().cairo_create()
-        ## Call our draw function to do stuff.
-        geom = self.get_window().get_geometry()
-        self.draw(geom.width, geom.height)
+# -----------------------------------------------------------------------------
 
-class MyStuff(Screen):
-    """This class is also a Drawing Area, coming from Screen."""
+
+class MqttAdapter(Thread):
+
     def __init__(self):
-        Screen.__init__(self)
-        ## x,y is where I'm at
-        self.x, self.y = 25, -25
-        ## rx,ry is point of rotation
-        self.rx, self.ry = -10, -25
-        ## rot is angle counter
-        self.rot = 0
-        ## sx,sy is to mess with scale
-        self.sx, self.sy = 1, 1
+        super(MqttAdapter, self).__init__()
+        self.client = mqtt.Client()
+        self.client.on_connect = self.on_connect
+        self.client.on_disconnect = self.on_disconnect
+        self.client.username_pw_set(username=Config.MQTT_BROKER_USER, password=Config.MQTT_BROKER_PASS)
+        self.client.connect(Config.MQTT_BROKER_ADDR, Config.MQTT_BROKER_PORT)
+        self.client.subscribe('surveillance/moocow/stream')
+        self.client.message_callback_add('surveillance/moocow/stream', self.on_stream)
+        self.display = None
 
-    def draw(self, width, height):
-        ## A shortcut
-        cr = self.cr
+    def set_display(self, display):
+        self.display = display
 
-        ## First, let's shift 0,0 to be in the center of page
-        ## This means:
-        ##  -y | -y
-        ##  -x | +x
-        ## ----0------
-        ##  -x | +x
-        ##  +y | +y
+    def on_connect(self, client, userdata, flags, rc):
+        print('Control connected..')
 
-        matrix = cairo.Matrix(1, 0, 0, 1, width/2, height/2)
-        cr.transform(matrix) # Make it so...
+    def on_disconnect(self, client, userdata, rc):
+        print('Control disconnected..')
 
-        ## Now save that situation so that we can mess with it.
-        ## This preserves the last context(the one at 0,0)
-        ## and let's us do new stuff.
-        cr.save()
+    def on_stream(self, client, userdata, msg):
+        print('Control received stream frame @ %s' % msg.topic)
+        if self.display is not None:
+            loader = GdkPixbuf.PixbufLoader()
+            loader.write(msg.payload)
+            loader.close()
+            pixbuf = loader.get_pixbuf()
+            self.display.set_image(pixbuf)
 
-        ## Now attempt to rotate something around a point
-        ## Use a matrix to change the shape's position and rotation.
+    def run(self):
+        print('MqttAdapter loop..')
+        self.client.loop_forever()
+        print('MqttAdapter loop done..')
 
-        ## First, make a matrix. Don't look at me, I only use this stuff :)
-        ThingMatrix = cairo.Matrix(1, 0, 0, 1, 0, 0)
-
-        ## Next, move the drawing to it's x,y
-        cairo.Matrix.translate(ThingMatrix, self.x, self.y)
-        cr.transform(ThingMatrix) # Changes the context to reflect that
-
-        ## Now, change the matrix again to:
-        cairo.Matrix.translate(ThingMatrix, self.rx, self.ry) # move it all to point of rotation
-        cairo.Matrix.rotate(ThingMatrix, self.rot) # Do the rotation
-        cairo.Matrix.translate(ThingMatrix, -self.rx, -self.ry) # move it back again
-        cairo.Matrix.scale(ThingMatrix, self.sx, self.sy) # Now scale it all
-        cr.transform(ThingMatrix) # and commit it to the context
-
-        ## Now, whatever is draw is "under the influence" of the
-        ## context and all that matrix magix we just did.
-        self.drawCairoStuff(cr)
-
-        ## Let's inc the angle a little
-        self.rot += 0.1
-
-        ## Now mess with scale too
-        self.sx += 0 # Change to 0 to see if rotation is working...
-        if self.sx > 4: self.sx=0.5
-        self.sy = self.sx
-
-        ## We restore to a clean context, to undo all that hocus-pocus
-        cr.restore()
-
-        ## Let's draw a crosshair so we can identify 0,0
-        ## Drawn last to be above the red square.
-        self.drawcross(cr)
-
-    def drawCairoStuff(self, cr):
-        ## Thrillingly, we draw a red rectangle.
-        ## It's drawn such that 0,0 is in it's center.
-        cr.rectangle(-25, -25, 50, 50)
-        cr.set_source_rgb(1, 0, 0)
-        cr.fill()
-        ## Now a visual indicator of the point of rotation
-        ## I have no idea(yet) how to keep this as a
-        ## tiny dot when the entire thing scales.
-        cr.set_source_rgb(1, 1, 1)
-        cr.move_to(self.rx, self.ry)
-        cr.line_to(self.rx+1, self.ry+1)
-        cr.stroke()
-
-    def drawcross(self, ctx):
-        ## Also drawn around 0,0 in the center
-        ctx.set_source_rgb(0, 0, 0)
-        ctx.move_to(0,10)
-        ctx.line_to(0, -10)
-        ctx.move_to(-10, 0)
-        ctx.line_to(10, 0)
-        ctx.stroke()
+# -----------------------------------------------------------------------------
 
 
-def run(Widget):
-    window = Gtk.Window()
-    window.connect("destroy", Gtk.main_quit)
-    window.set_size_request(400, 400)
-    widget = Widget()
-    widget.show()
-    window.add(widget)
-    window.present()
+if __name__ == '__main__':
+    # Setup
+    display = Display()
+    mqtt_adapter = MqttAdapter()
+    mqtt_adapter.set_display(display)
+    # Run
+    mqtt_adapter.start()
     Gtk.main()
-
-run(MyStuff)
+    # Cleanup
+    mqtt_adapter.client.disconnect()
+    mqtt_adapter.join()
